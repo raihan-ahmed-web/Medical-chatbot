11880c469849ac098b1e8dc572f1dedfbfb05a3c
raihan-ahmed-web <hasansemanto704@gmail.com>
2025-04-11 02:35:00 +0600

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Raihan Ahmed',
+    author_email= 'ahmed15-5737@diu.edu.bd',
+    packages= find_packages(),
+    install_requires = []
+
+)
